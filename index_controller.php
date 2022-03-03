2ca61e51d0a7dce8fb9c5f349271ec1092f80469
marciofao <marcio.lopes.fao@gmail.com>
2022-03-02 22:29:06 -0300

    Naming convention update

--- index_controller.php
+++ index_controller.php
@@ -4,15 +4,13 @@
 @session_start(); // Inicializa a sessão
 
 //se a sessão estiver iniciada encaminha para o inicio
-if (isset($_SESSION["usuario"]))
-{
-	header("location:inicio.php?c=1");
-}
+if (isset($_SESSION["usuario"])) header("location:inicio.php?c=1");
+
 
 //verifica credenciais
 if ($_POST) {
 	$usuario=strtolower($_POST['usuario']);
-	require_once 'php_assets/conecta.php';
+	require_once 'php_assets/config.php';
 	//var_dump($_POST); die();
 
 	$data=$database->select('usuarios_info', '*', ["usuario"=>$usuario]);
@@ -21,18 +19,21 @@ if ($_POST) {
 
 	if ($data) { //se existir o usuario
 
+		$data = $data[0];
+		//dump_die($data);
 		//verifica se a senha esta correta
-		if ($data[0]['senha']==$_POST['senha']) {
+		if ($data['senha']==$_POST['senha']) {
 			
 			//configura sessão
 			
 			
-			$_SESSION["cod"]=$data[0]['cod'];
-			$_SESSION["usuario"]=$data[0]['usuario'];
-			$_SESSION["nome"]=$data[0]['nome'];
-			$_SESSION["email"]=$data[0]['email'];
-			$_SESSION["email_destino"]=$data[0]['email_destino'];
-			$_SESSION["envia_copia"]=$data[0]['envia_copia'];
+			$_SESSION["cod"]=$data['cod'];
+			$_SESSION["usuario"]=$data['usuario'];
+			$_SESSION["nome"]=$data['nome'];
+			$_SESSION["email"]=$data['email'];
+			$_SESSION["email_destino"]=$data['email_destino'];
+			$_SESSION["envia_copia"]=$data['envia_copia'];
+			$_SESSION["user_level"]=$data['user_level'];
 			//die($_SESSION['nome']);
 
 			//CRIA REGISTRO DE ULTIMO ACESSO
